33bdfc6628c7fdbb14ba0b83f9b219b7154d87a3
Iburinoc <iburinoc@gmail.com>
2015-02-14 18:45:56 -0500

    added tcp keepalive to server, and removed unnecessary select

--- inet/chatserver.c
+++ inet/chatserver.c
@@ -1,15 +1,16 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
 #include <fcntl.h>
 #include <unistd.h>
 #include <signal.h>
 
 #include <sys/socket.h>
 #include <sys/types.h>
-#include <sys/select.h>
 #include <sys/time.h>
 #include <netinet/in.h>
+#include <netinet/tcp.h>
 #include <arpa/inet.h>
 #include <netdb.h>
 
@@ -17,47 +18,55 @@
 
 #define PORT "3490"
 
+#ifdef __linux__
+# define TCP_KEEPALIVE_IDLE TCP_KEEPIDLE
+# define TCP_KEEPALIVE_INTERVAL TCP_KEEPINTVL
+# define TCP_KEEPALIVE_COUNT TCP_KEEPCNT
+#endif
+#if defined(__APPLE__) && defined(__MACH__)
+# define TCP_KEEPALIVE_IDLE TCP_KEEPALIVE
+# define TCP_KEEPALIVE_INTERVAL TCP_KEEPINTVL
+# define TCP_KEEPALIVE_COUNT TCP_KEEPCNT
+#endif
+
 void *handle_connection(void *arg) {
 	int sockfd = *((int *)arg);
-
-	fd_set set;
-	struct timeval wait;
 
 	const size_t BUFSIZE = 1024;
 	char buf[BUFSIZE + 1];
 	buf[BUFSIZE] = '\0';
 
-	wait.tv_sec = 0;
-	wait.tv_usec = 50000;
-
 	printf("new connection handler opened for sockfd %d\n", sockfd);
 	while(1) {
-		FD_ZERO(&set);
-		FD_SET(sockfd, &set);
-		select(FD_SETSIZE, &set, NULL, &set, &wait);
-
 		ssize_t read = recv(sockfd, buf, BUFSIZE, 0);
 		if(read == -1) {
 			perror("read error");
 		} else if(read == 0) {
 			close(sockfd);
-			FD_ZERO(&set);
 			printf("handler for sockfd %d exiting\n", sockfd);
 			return NULL;
-		}
-		buf[read] = '\0';
+		} else {
+			buf[read] = '\0';
 
-		size_t total = 0;
-		while(total < read) {
-			ssize_t written = send(sockfd, &buf[total], read - total, 0);
-			if(written == -1) {
-				perror("write error");
-				printf("%d, %zu, %zd, %s\n", sockfd, total, read, buf);
+			size_t total = 0;
+			while(total < read) {
+				ssize_t written = send(sockfd, &buf[total], read - total, 0);
+				if(written == -1) {
+					perror("write error");
+					printf("%d, %zu, %zd, %s\n", sockfd, total, read, buf);
+				}
+				total += written;
 			}
-			total += written;
-		}
 
-		printf("%d: %s", sockfd, buf);
+			printf("%d: ", sockfd);
+			size_t i;
+			for(i = 0; i < read; i++) {
+				if(!iscntrl(buf[i])) {
+					putchar(buf[i]);
+				}
+			}
+			puts("");
+		}
 	}
 }
 
@@ -139,6 +148,23 @@ int main(int argc, char **argv) {
 			continue;
 		}
 		printf("server: connection from %s\n", s);
+
+		int idle = 1;
+		int intvl = 1;
+		int cnt = 4;
+		int yes = 1;
+		if(setsockopt(new_fd, IPPROTO_TCP, TCP_KEEPALIVE_IDLE, &idle, sizeof(idle)) != 0) {
+			perror("server: setsockopt");
+		}
+		if(setsockopt(new_fd, IPPROTO_TCP, TCP_KEEPALIVE_INTERVAL, &intvl, sizeof(intvl)) != 0) {
+			perror("server: setsockopt");
+		}
+		if(setsockopt(new_fd, IPPROTO_TCP, TCP_KEEPALIVE_COUNT, &cnt, sizeof(cnt)) != 0) {
+			perror("server: setsockopt");
+		}
+		if(setsockopt(new_fd, SOL_SOCKET, SO_KEEPALIVE, &yes, sizeof(yes)) != 0) {
+			perror("server: setsockopt");
+		}
 
 		pthread_t thread;
 
